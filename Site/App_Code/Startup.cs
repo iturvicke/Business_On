cb334002863d3f7a00d029b5dcf4a2c8b6f4c396
iturvicke <iturvicque@hotmaill.es>
2017-08-19 14:48:54 -0500

    Add New WebSite

---
+++ Site/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Business_.Startup))]
+namespace Business_
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
